1bff369349ace981515004f36e66c6d8515df965
utuedey <utuedeyejosph@gmail.com>
2023-07-07 02:48:34 +0000

    use typing.list[]

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,11 +2,11 @@
 
 """
 5-sum_list module
-Takes a list input_list of floats as argument and returns their sum as a float
+Takes a list input_list of floats as argument and returns their sum as a float.
 """
+import typing
 
-
-new_input_list = [float]
+new_input_list = typing.List[float]
 
 
 def sum_list(input_list: new_input_list) -> float:
