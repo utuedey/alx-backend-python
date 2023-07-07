68db24f7c365fa049ee186dc663ad42e6a63d3e4
utuedey <utuedeyejosph@gmail.com>
2023-07-07 02:00:20 +0000

    Add task 4

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""
+4-define_variables module - Define and annotate the following variables with the specified values
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
