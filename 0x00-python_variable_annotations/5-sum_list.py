76c25fd14dc1aae4635ac44bade66fe8a8e68703
utuedey <utuedeyejosph@gmail.com>
2023-07-07 02:23:50 +0000

    Add task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+5-sum_list module
+Takes a list input_list of floats as argument and returns their sum as a float
+"""
+
+
+new_input_list = [float]
+
+def sum_list(input_list: new_input_list) -> float:
+    """Return the sum of a list of float as float"""
+    return sum([x for x in input_list])
+
