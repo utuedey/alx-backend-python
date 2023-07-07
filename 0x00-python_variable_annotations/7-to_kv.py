c6582995f0ab6c65f6c7a0bc6e4a6acad0df7a42
utuedey <utuedeyejosph@gmail.com>
2023-07-07 03:01:34 +0000

    Add task 7

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+7-to_kv module
+Takes a string k and an int OR float v as arguments and returns a tuple.
+"""
+import typing
+
+
+v_type = typing.Union[int, float]
+r_type = typing.Tuple[str, float]
+
+def to_kv(k: str, v: v_type) -> r_type:
+    """Return a tuple of k and square of v."""
+    return k, v**2
