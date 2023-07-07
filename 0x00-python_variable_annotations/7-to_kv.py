4d987139b7a999e6f59f9aa7342f26bb3aea088c
utuedey <utuedeyejosph@gmail.com>
2023-07-07 03:03:05 +0000

    add blank line

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -10,6 +10,7 @@ import typing
 v_type = typing.Union[int, float]
 r_type = typing.Tuple[str, float]
 
+
 def to_kv(k: str, v: v_type) -> r_type:
     """Return a tuple of k and square of v."""
     return k, v**2
