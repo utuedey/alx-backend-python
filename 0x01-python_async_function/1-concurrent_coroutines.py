242730c18fce06866d2196100238e72a881e810f
utuedey <utuedeyejosph@gmail.com>
2023-07-10 19:19:59 +0000

    update task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -15,7 +15,8 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
        Return:
        - list of all the delays (float values).
     """
-    res = await asyncio.gather(*(map(lambda _: wait_random(max_delay), range(n))))
+    res = await asyncio.gather(*tuple(map(lambda _: wait_random(max_delay),
+                                          range(n))))
     return sorted(res)
 
 
