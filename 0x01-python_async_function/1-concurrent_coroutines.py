58b75f54ce5e6c9b95756f53dbb2a6bccbaa9b93
utuedey <utuedeyejosph@gmail.com>
2023-07-10 19:24:00 +0000

    edit task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -18,7 +18,3 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     res = await asyncio.gather(*tuple(map(lambda _: wait_random(max_delay),
                                           range(n))))
     return sorted(res)
-
-
-if __name__ == "__main__":
-    asyncio.run(wait_n(5, 5))
