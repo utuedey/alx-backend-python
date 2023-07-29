d5a4f95f1ca3ebe9a7cb2291f22db43e469b7c7d
utuedey <utuedeyejosph@gmail.com>
2023-07-29 22:44:49 +0000

    add test_utils.py

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""test cases for utils module"""
+
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Test cases for the utils functions"""
+
+    # Define the test data as a list of tuples:
+    # (input_a, input_b, expected_result)
+    test_parameters = (
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+        )
+
+    @parameterized.expand(test_parameters)
+    def test_access_nested_map(self, nested_map, path, expected_result):
+        """Test case for the access_nested_map function"""
+        res = access_nested_map(nested_map, path)
+        self.assertEqual(res, expected_result)
+
+
+if __name__ == "__main__":
+    unittest.main()
