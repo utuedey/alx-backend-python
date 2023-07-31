661473c0c862299fc2fc4cc74193012eab1e039a
utuedey <utuedeyejosph@gmail.com>
2023-07-31 17:38:27 +0000

    add memoize test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -7,7 +7,8 @@ from parameterized import parameterized
 
 from utils import (
     access_nested_map,
-    get_json
+    get_json,
+    memoize
     )
 
 
@@ -56,6 +57,29 @@ class TestGetJson(unittest.TestCase):
         with patch("requests.get", return_value=Mock(**atrributes)) as req_get:
             self.assertEqual(get_json(test_url), test_payload)
             req_get.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    """Test the memoize function"""
+
+    def test_memoize(self):
+        """Test memoize output"""
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, "a_method",
+                          return_value=lambda: 42,) as memoize_func:
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property(), 42)
+            self.assertEqual(test_class.a_property(), 42)
+
+            memoize_func.assert_called_once()
 
 
 if __name__ == "__main__":
