0feee5ada3be43291f8ffe5e8444fffa6620f258
utuedey <utuedeyejosph@gmail.com>
2023-08-05 03:50:59 +0000

    add test_public_repos

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -46,6 +46,57 @@ class TestGithubOrgClient(unittest.TestCase):
             self.assertEqual(GithubOrgClient("google")._public_repos_url,
                              "https://api.github.com/users/google/repos")
 
+    @patch("client.get_json")
+    def test_public_repos(self, mock_get_json: MagicMock) -> None:
+        """test for the public_repos method"""
+        test_payload = {
+            "repos_url": "https://api.github.com/orgs/google/repos",
+            "repos": [
+                {
+                 "id": 7776515,
+                 "name": "cpp-netlib",
+                 "private": False,
+                 "owner": {
+                        "login": "google",
+                        "id": 1342004,
+                        },
+                 "fork": True,
+                 "url": "https://api.github.com/repos/google/cpp-netlib",
+                 "created_at": "2013-01-23T14:45:32Z",
+                 "updated_at": "2019-11-15T02:26:31Z",
+                 "has_issues": False,
+                 "forks": 59,
+                 "default_bracnch": "master",
+                    },
+                {
+                 "id": 7968417,
+                 "name": "dagger",
+                 "private": False,
+                 "owner": {
+                        "login": "google",
+                        "id": 1342004,
+                        },
+                 "fork": True,
+                 "url": "https://api.github.com/repos/google/dagger",
+                 "created_at": "2013-02-01T23:14:14Z",
+                 "updated_at": "2019-12-03T12:39:55Z",
+                 "has_issues": True,
+                 "forks": 1741,
+                 "default_bracnch": "master",
+                    },
+                ]
+            }
+        mock_get_json.return_value = test_payload['repos']
+        with patch(
+                "client.GithubOrgClient._public_repos_url",
+                new_callable=PropertyMock) as mock_public_repos_url:
+            mock_public_repos_url.return_value = test_payload["repos_url"]
+            self.assertEqual(
+                GithubOrgClient("google").public_repos(),
+                ["cpp-netlib", "dagger"])
+            mock_public_repos_url.assert_called_once()
+        mock_get_json.assert_called_once()
+
 
 if __name__ == "__main__":
     unittest.main()
